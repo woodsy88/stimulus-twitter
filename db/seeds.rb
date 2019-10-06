5cfd136d480d3098045a71ed4570d2afbb516793
“Andrew <andrewwoods88@gmail.com>
2019-10-06 11:23:47 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Tweet.destroy_all
+Event.destroy_all
+User.destroy_all
+
+andrew = User.create!(
+  email: "a@d.co",
+  password: "password",
+  password_confirmation: "password",
+  first_name: "Andrew",
+  last_name: "Woods",
+  username: "woodsy88"
+  )
+
+puts "1 user created"
+
+tweets = [
+  ["Non deserunt eu irure veniam occaecat Lorem mollit officia aliqua proident do Lorem. Pariatur deserunt ad aliqua excepteur aliquip enim adipisicing. Nostrud veniam dolor quis nisi cillum deserunt consectetur culpa incididunt sunt velit do. Id est enim sit tempor sunt consectetur est sint.", andrew.id],
+  ["Non deserunt eu irure veniam occaecat Lorem mollit officia aliqua proident do Lorem. Pariatur deserunt ad aliqua excepteur aliquip enim adipisicing. Nostrud veniam dolor quis nisi cillum deserunt consectetur culpa incididunt sunt velit do. Id est enim sit tempor sunt consectetur est sint.", andrew.id],
+  ["Non deserunt eu irure veniam occaecat Lorem mollit officia aliqua proident do Lorem. Pariatur deserunt ad aliqua excepteur aliquip enim adipisicing. Nostrud veniam dolor quis nisi cillum deserunt consectetur culpa incididunt sunt velit do. Id est enim sit tempor sunt consectetur est sint.", andrew.id],
+  ["Non deserunt eu irure veniam occaecat Lorem mollit officia aliqua proident do Lorem. Pariatur deserunt ad aliqua excepteur aliquip enim adipisicing. Nostrud veniam dolor quis nisi cillum deserunt consectetur culpa incididunt sunt velit do. Id est enim sit tempor sunt consectetur est sint.", andrew.id],
+  ["Non deserunt eu irure veniam occaecat Lorem mollit officia aliqua proident do Lorem. Pariatur deserunt ad aliqua excepteur aliquip enim adipisicing. Nostrud veniam dolor quis nisi cillum deserunt consectetur culpa incididunt sunt velit do. Id est enim sit tempor sunt consectetur est sint.", andrew.id],
+]
+
+
+tweets.each do |body, user_id|
+  Tweet.create!(body: body, user_id: user_id)
+end
+
